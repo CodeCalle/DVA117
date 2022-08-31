fb4e45d070fe9b748fb0a636782452e0d21f8bd1
Carl-Johan Höglind <chd16002@student.mdu.se>
2022-08-31 10:27:05 +0200

    Lab 2.1 complete

--- Block 1/Labb 2/main-2.1.c
+++ Block 1/Labb 2/main-2.1.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Något");
+    int degree;
+    // scanf("Enter a positive angle in degrees: %d", &degree);
+    printf("Enter a positive angle in degrees: ");
+    scanf("%d", &degree);
+
+    if (degree < 90)
+        printf("The angle is acute!");
+    else if (degree < 180 && 90 < degree)
+        printf("The angle is obtuse!");
+    else if (degree == 90)
+        printf("The angle is right!");
+    else
+        printf("Wrongful input!");
+
+    return 0;
 }
